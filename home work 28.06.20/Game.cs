f5bcdad40cdbb00f88934aba50596908f16abbd3
DanielLevi3 <66633430+DanielLevi3@users.noreply.github.com>
2020-06-29 19:22:53 +0300

    Add files via upload

---
+++ home work 28.06.20/Game.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._06._20_Hw
+{
+   public  class Game
+    {
+        public string _name;
+        public int _numberOfPlayers;
+        public float _rating;
+        public string _originCountry;
+
+
+
+        public Game()
+        {
+            
+           
+        }
+
+        public Game(string name, float rating = 5.5f)
+        {
+            _name = name;
+            _rating = rating;
+     
+        }
+
+        public Game(string name, int numberOfPlayers, float rating, string originCountry)
+        {
+            _name = name;
+            _numberOfPlayers = numberOfPlayers;
+            _rating = rating;
+            _originCountry = originCountry;
+        }
+
+        public Game(string name, string originCountry)
+        {
+            _name = name;
+            _originCountry = originCountry;
+        }
+
+
+        public int TellMeHowManyPlayers()
+        {
+
+            return this._numberOfPlayers;
+
+        }
+
+        public override string ToString()
+        {
+            return $"name: {_name} numberOfPlayers : {_numberOfPlayers} originCountry: {_originCountry} rating : {_rating} ";
+        }
+    }
+}
---
+++ home work 28.06.20/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._06._20_Hw
+{
+
+    class Program
+    {
+
+
+        static void PrintGame(Game g)
+        {
+            Console.WriteLine(g);
+            int number = g.TellMeHowManyPlayers();
+            Console.WriteLine(number);
+        
+        }
+        static void Main(string[] args)
+        {
+            Game Damka = new Game()
+            {
+                _name = "Backgammon",
+                _numberOfPlayers = 2,
+                _originCountry = "China",
+                _rating = 3.5f
+
+
+            };
+
+            Game Ball = new Game("Basketball", 5.5f)
+            {
+                _numberOfPlayers = 10,
+                _originCountry = "USA"
+            };
+
+            Game Ball2 = new Game("Soccer", 22, 5.5f, "England");
+
+            Game T = new Game("GameBoy", "Japan")
+            {
+                _numberOfPlayers = 1,
+                _rating = 4.1f
+            };
+
+
+            PrintGame(Ball);
+            // Home Work answers:
+            // * the command in IL is "newobj"
+            // * yes because its static function which we know that has to be the letter 's' next to it 
+            //* call , callvirt
+            //* I think thats happen because cw is simpler function that dosen't need to called in a specefied way 
+            //  and "TellMeHowManyPlayers()" probbly needed to be call in a more specefied way. 
+                                            
+
+                
+        }
+    }
+}
